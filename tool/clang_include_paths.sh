b710739adef9e73ab24acd04ebe44c80329884ff
Timo Nicolai <timonicolai@arcor.de>
2020-12-07 08:01:44 +0000

    Allow specifying clang version at build time

--- tool/clang_include_paths.sh
+++ tool/clang_include_paths.sh
@@ -3,7 +3,7 @@
 INCLUDE_START="#include <...> search starts here:"
 INCLUDE_END="End of search list"
 
-clang_verbose=$(clang++ -v -E -xc++ /dev/null 2>&1 >/dev/null)
+clang_verbose=$($CLANGXX -v -E -xc++ /dev/null 2>&1 >/dev/null)
 
 clang_include_paths=$(echo "$clang_verbose" | sed -n "/$INCLUDE_START/,/$INCLUDE_END/p" | head -n-1 | tail -n+2)
 
