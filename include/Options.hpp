8457cf5581ab957a10ecfa73ebc5f074b53f3310
Timo Nicolai <timonicolai@arcor.de>
2021-02-24 12:20:21 +0100

    Multi valued options
    It is now possible to e.g. create options of type std::vector<std::string>
    which will accept multiple comma separated values and automatically store them
    in a vector.

--- include/Options.hpp
+++ include/Options.hpp
@@ -9,6 +9,8 @@
 #include <optional>
 #include <string>
 #include <tuple>
+#include <type_traits>
+#include <experimental/type_traits>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -16,6 +18,7 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Casting.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorHandling.h"
 
@@ -32,8 +35,19 @@ using OptionChoices =
 
 class OptionsRegistry : private mixin::NotCopyOrMoveable
 {
-  friend void initOptions();
   friend OptionsRegistry &Options();
+
+  template<typename U>
+  using value_push_back_t =
+    decltype(std::declval<U>().push_back(std::declval<typename U::value_type>()));
+
+  template<typename U>
+  static inline constexpr bool has_value_push_back_v =
+    std::experimental::is_detected_v<value_push_back_t, U>;
+
+  template<typename U>
+  static inline constexpr bool is_container_v =
+    has_value_push_back_v<U> && !std::is_same_v<U, std::string>;
 
   template<typename T = std::string>
   class Option
@@ -58,29 +72,30 @@ class OptionsRegistry : private mixin::NotCopyOrMoveable
       return *this;
     }
 
-    Option &setOptional(bool Optional)
-    { Optional_ = Optional; return *this; }
-
     Option &setChoices(OptionChoices<T> Choices)
     { Choices_ = Choices; return *this; }
 
     Option &setDefault(T const &Default)
     { Default_ = Default; return *this; }
 
+    Option &setOptional(bool Optional)
+    { Optional_ = Optional; return *this; }
+
     Option &addAssertion(Assertion &&Assert, std::string const &Msg)
     { Assertions_.emplace_back(Assert, Msg); return *this; }
 
     void done() const
-    { OptionsRegistry::instance().addOpt(*this); }
+    { OptionsRegistry::instance().addOption(*this); }
 
   private:
     llvm::StringRef Name_;
 
     std::optional<llvm::StringRef> Desc_;
     std::optional<llvm::StringRef> ValueDesc_;
-    std::optional<bool> Optional_;
     std::optional<OptionChoices<T>> Choices_;
     std::optional<T> Default_ = std::nullopt;
+
+    bool Optional_ = true;
 
     std::vector<std::pair<Assertion, std::string>> Assertions_;
   };
@@ -89,18 +104,42 @@ public:
   static void init();
 
   template<typename T = std::string>
-  T get(llvm::StringRef Name) const
+  std::enable_if_t<!is_container_v<T>, T>
+  get(llvm::StringRef Name) const
   {
-    T Value = *instance().findOpt<T>(Name);
+    auto Opt_(instance().findOption(Name));
 
-    assertOptValue(Name, Value);
+    auto Opt(std::dynamic_pointer_cast<llvm::cl::opt<T>>(Opt_));
+    if (!Opt)
+      throw CPPBindError(ErrorMsg() << Name.str() << ": invalid type");
+
+    T Value = *Opt;
+
+    assertOptionValue(Name, Value);
 
     return Value;
   }
 
   template<typename T>
-  void set(llvm::StringRef Name, T const &Value) const
-  { *findOpt<T>(Name) = Value; }
+  std::enable_if_t<is_container_v<T>, T>
+  get(llvm::StringRef Name) const
+  {
+    using V = typename T::value_type;
+
+    auto Opt_(instance().findOption(Name));
+
+    auto Opt(std::dynamic_pointer_cast<llvm::cl::list<V>>(Opt_));
+    if (!Opt)
+      throw CPPBindError(ErrorMsg() << Name.str() << ": invalid type");
+
+    T Values;
+    for (auto const &Value : *Opt)
+      Values.push_back(Value);
+
+    assertOptionValue(Name, Values);
+
+    return Values;
+  }
 
   clang::tooling::CommonOptionsParser parser(int Argc, char const **Argv)
   { return {Argc, Argv, Category_}; }
@@ -126,14 +165,14 @@ private:
   { return Option<T>(Name); }
 
   template<typename T>
-  void addOpt(Option<T> Option)
+  void addOption(Option<T> Option)
   {
     // XXX aliases
     // XXX indicate mandatory options and default values in Desc
 
-    assert(Opts_.find(Option.Name_.str()) == Opts_.end());
+    auto Opt(createOption<T>(Option.Name_, llvm::cl::cat(Category_)));
 
-    auto Opt(packOpt<T>(Option.Name_, llvm::cl::cat(Category_)));
+    Opts_[Option.Name_.str()] = Opt;
 
     if (Option.Desc_)
       Opt->setDescription(*Option.Desc_);
@@ -141,61 +180,77 @@ private:
     if (Option.ValueDesc_)
       Opt->setValueStr(*Option.ValueDesc_);
 
-    if (Option.Optional_)
-      Opt->setNumOccurrencesFlag(*Option.Optional_ ? llvm::cl::Optional
-                                                   : llvm::cl::Required);
+    if constexpr (is_container_v<T>) {
+      Opt->setNumOccurrencesFlag(Option.Optional_ ? llvm::cl::Optional
+                                                  : llvm::cl::OneOrMore);
 
-    if constexpr (std::is_same_v<T, bool>)
-      Opt->setValueExpectedFlag(*Option.Optional_ ? llvm::cl::ValueOptional
-                                                  : llvm::cl::ValueRequired);
+    } else {
+      Opt->setNumOccurrencesFlag(Option.Optional_ ? llvm::cl::Optional
+                                                  : llvm::cl::Required);
 
-    if (Option.Default_)
-      Opt->setInitialValue(*Option.Default_);
-
-    if (Option.Choices_) {
-      if constexpr (std::is_enum_v<T>) {
-        for (auto const &Choice : *Option.Choices_) {
-          Opt->getParser().addLiteralOption(std::get<0>(Choice),
-                                            std::get<1>(Choice),
-                                            std::get<2>(Choice));
-        }
-      } else {
-        assert(false && "multi-choice options must be of enum type");
+      if constexpr (std::is_same_v<T, bool>) {
+        Opt->setValueExpectedFlag(Option.Optional_ ? llvm::cl::ValueOptional
+                                                   : llvm::cl::ValueRequired);
       }
-    }
 
-    Opts_[Option.Name_.str()] = Opt;
+      auto OptSingleValued(std::dynamic_pointer_cast<llvm::cl::opt<T>>(Opt));
+
+      if (Option.Choices_) {
+        if constexpr (std::is_enum_v<T>) {
+          for (auto const &Choice : *Option.Choices_) {
+            OptSingleValued->getParser().addLiteralOption(std::get<0>(Choice),
+                                                          std::get<1>(Choice),
+                                                          std::get<2>(Choice));
+          }
+        } else {
+          assert(false && "multi-choice options must be of enum type");
+        }
+      }
+
+      if (Option.Default_)
+        OptSingleValued->setInitialValue(*Option.Default_);
+    }
 
     if (!Option.Assertions_.empty())
       OptAssertions_[Option.Name_.str()] = Option.Assertions_;
   }
 
-  template<typename T>
-  auto findOpt(llvm::StringRef Name) const
+  template<typename T, typename ...ARGS>
+  std::enable_if_t<!is_container_v<T>, std::shared_ptr<llvm::cl::Option>>
+  createOption(llvm::StringRef Name, ARGS&&... Args)
+  {
+    auto Opt(std::make_shared<llvm::cl::opt<T>>(Name,
+                                                std::forward<ARGS>(Args)...));
+
+    Opts_[Name.str()] = Opt;
+
+    return Opt;
+  }
+
+  template<typename T, typename ...ARGS>
+  std::enable_if_t<is_container_v<T>, std::shared_ptr<llvm::cl::Option>>
+  createOption(llvm::StringRef Name, ARGS&&... Args)
+  {
+    using V = typename T::value_type;
+
+    auto Opt(std::make_shared<llvm::cl::list<V>>(Name,
+                                                 llvm::cl::CommaSeparated,
+                                                 std::forward<ARGS>(Args)...));
+
+    return Opt;
+  }
+
+  std::shared_ptr<llvm::cl::Option> findOption(llvm::StringRef Name) const
   {
     auto OptIt(Opts_.find(Name.str()));
 
     assert(OptIt != Opts_.end() && "valid option name");
 
-    return unpackOpt<T>(OptIt->second);
-  }
-
-  template<typename T, typename ...ARGS>
-  static auto packOpt(ARGS&&... Args)
-  { return std::make_shared<llvm::cl::opt<T>>(std::forward<ARGS>(Args)...); }
-
-  template<typename T>
-  static auto unpackOpt(std::any const &Opt)
-  {
-    try {
-      return std::any_cast<std::shared_ptr<llvm::cl::opt<T>>>(Opt);
-    } catch (std::bad_any_cast const &) {
-      llvm_unreachable("invalid option type");
-    }
+    return OptIt->second;
   }
 
   template<typename T>
-  void assertOptValue(llvm::StringRef Name, T const &Value) const
+  void assertOptionValue(llvm::StringRef Name, T const &Value) const
   {
     auto It(OptAssertions_.find(Name.str()));
     if (It == OptAssertions_.end())
@@ -220,17 +275,9 @@ private:
   std::string Usage_;
   std::vector<llvm::cl::extrahelp> Help_;
 
-  std::unordered_map<std::string, std::any> Opts_;
+  std::unordered_map<std::string, std::shared_ptr<llvm::cl::Option>> Opts_;
   std::unordered_map<std::string, std::any> OptAssertions_;
 };
-
-template<>
-inline llvm::StringRef OptionsRegistry::get<llvm::StringRef>(
-  llvm::StringRef Name) const
-{
-  auto Opt(instance().findOpt<std::string>(Name));
-  return Opt->c_str();
-}
 
 inline OptionsRegistry &Options()
 { return OptionsRegistry::instance(); }
