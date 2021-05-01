2c05bc4f1cf73dee1ccea3387dc7c15af8362dba
Timo Nicolai <timonicolai@arcor.de>
2021-05-01 21:12:02 +0200

    Rust: finish default argument test

--- backend/test/c/test_default_arguments.c
+++ backend/test/c/test_default_arguments.c
@@ -12,21 +12,21 @@ int main()
   assert(test_default_constexpr_function_call(test_func_constexpr()) == test_func_constexpr());
   assert(test_default_function_call(test_func()) == test_func());
 
-//  int a;
-//  test_default_ref(&a, 42);
-//  assert(a == 42);
-//
-//  {
-//    struct test_a_class a_class = test_a_class_new(42);
-//    struct test_a_class a_class_copy = test_default_record(&a_class);
-//    assert(test_a_class_get_value(&a_class_copy) == 42);
-//
-//    test_a_class_delete(&a_class);
-//    test_a_class_delete(&a_class_copy);
-//  }
-//
-//  assert(test_default_template_unsigned_int(1u) == 1u);
-//  assert(test_default_template_after_parameter_pack_unsigned_int_int_int(1, 2, 3u) == 3u);
-//
-//  return 0;
+  int a;
+  test_default_ref(&a, 42);
+  assert(a == 42);
+
+  {
+    struct test_a_class a_class = test_a_class_new(42);
+    struct test_a_class a_class_copy = test_default_record(&a_class);
+    assert(test_a_class_get_value(&a_class_copy) == 42);
+
+    test_a_class_delete(&a_class);
+    test_a_class_delete(&a_class_copy);
+  }
+
+  assert(test_default_template_unsigned_int(1u) == 1u);
+  assert(test_default_template_after_parameter_pack_unsigned_int_int_int(1, 2, 3u) == 3u);
+
+  return 0;
 }
--- backend/test/rust/test_default_arguments.rs
+++ backend/test/rust/test_default_arguments.rs
@@ -10,5 +10,18 @@ fn main() {
     assert!(test::default_constexpr_function_call(test::func_constexpr()) == test::func_constexpr());
     assert!(test::default_function_call(test::func()) == test::func());
 
+    let mut a: i32 = 0;
+    test::default_ref(&mut a, 42);
+    assert!(a == 42);
+
+    {
+        let mut a_class = test::AClass::new(42);
+        let a_class_copy = test::default_record(&mut a_class);
+        assert!(a_class_copy.get_value() == 42);
+    }
+
+    assert!(test::default_template_unsigned_int(1) == 1);
+    assert!(test::default_template_after_parameter_pack_unsigned_int_int_int(1, 2, 3) == 3);
+
     }
 }
