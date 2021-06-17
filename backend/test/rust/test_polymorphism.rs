02a09ba18d7f6fb628552ddc7a37ab4b4e2ac03c
Timo Nicolai <timonicolai@arcor.de>
2021-06-17 11:17:51 +0200

    Rust: test_polymorphism.rs: style fix

--- backend/test/rust/test_polymorphism.rs
+++ backend/test/rust/test_polymorphism.rs
@@ -10,18 +10,14 @@ fn main() {
     let derived_2 = test::Derived2::new();
 
     {
-      let mut v: Vec<&dyn Base1Trait> = vec![];
-      v.push(&derived_1);
-      v.push(&derived_2);
+      let v: Vec<&dyn Base1Trait> = vec![&derived_1, &derived_2];
 
       assert!(test::func_1(v[0]) == 1);
       assert!(test::func_1(v[1]) == 2);
     }
 
     {
-      let mut v: Vec<&dyn Base2Trait> = vec![];
-      v.push(&derived_1);
-      v.push(&derived_2);
+      let v: Vec<&dyn Base2Trait> = vec![&derived_1, &derived_2];
 
       assert!(test::func_2(v[0]) == -1);
       assert!(test::func_2(v[1]) == -2);
