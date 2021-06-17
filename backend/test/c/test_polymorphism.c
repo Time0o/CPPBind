39bece409de8a2a6e13d6fb0bbc8846e2c579394
Timo Nicolai <timonicolai@arcor.de>
2021-06-17 11:27:05 +0200

    C: test_polymorphism.c: clean up

--- backend/test/c/test_polymorphism.c
+++ backend/test/c/test_polymorphism.c
@@ -8,21 +8,21 @@ int main()
   struct test_derived_2 derived_2 = test_derived_2_new();
 
   {
-    struct test_base_1 base_1_arr[2];
-    base_1_arr[0] = test_derived_1_cast_to_const_base_1(&derived_1);
-    base_1_arr[1] = test_derived_2_cast_to_const_base_1(&derived_2);
+    struct test_base_1 v[2];
+    v[0] = test_derived_1_cast_to_const_base_1(&derived_1);
+    v[1] = test_derived_2_cast_to_const_base_1(&derived_2);
 
-    assert(test_base_1_func_1(&base_1_arr[0]) == 1);
-    assert(test_base_1_func_1(&base_1_arr[1]) == 2);
+    assert(test_func_1(&v[0]) == 1);
+    assert(test_func_1(&v[1]) == 2);
   }
 
   {
-    struct test_base_2 base_2_arr[2];
-    base_2_arr[0] = test_derived_1_cast_to_const_base_2(&derived_1);
-    base_2_arr[1] = test_derived_2_cast_to_const_base_2(&derived_2);
+    struct test_base_2 v[2];
+    v[0] = test_derived_1_cast_to_const_base_2(&derived_1);
+    v[1] = test_derived_2_cast_to_const_base_2(&derived_2);
 
-    assert(test_base_2_func_2(&base_2_arr[0]) == -1);
-    assert(test_base_2_func_2(&base_2_arr[1]) == -2);
+    assert(test_func_2(&v[0]) == -1);
+    assert(test_func_2(&v[1]) == -2);
   }
 
   test_derived_1_delete(&derived_1);
